39597f33e914beb1a42fb9ac47ba47c8f2a9f7c7
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-29 10:43:19 +0530

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import _ from 'lodash';
 import './style.css';
 
-  const todos = [
-    { index: 1, description: 'Learn JavaScript', completed: false  },
-    { index: 2, description: 'Learn HTML', completed: true },
-    { index: 3, description: 'Learn CSS', completed: false },
-    { index: 4, description: 'Learn React', completed: true },
-    { index: 5, description: 'Learn Redux', completed: false },
-  ]
+const todos = [
+  { index: 1, description: 'Learn JavaScript', completed: false },
+  { index: 2, description: 'Learn HTML', completed: true },
+  { index: 3, description: 'Learn CSS', completed: false },
+  { index: 4, description: 'Learn React', completed: true },
+  { index: 5, description: 'Learn Redux', completed: false },
+];
 
-  const taskContainer = document.querySelector('.body-container');
+const taskContainer = document.querySelector('.body-container');
 
-  var tasks = todos.map((todo) => ` 
+const tasks = todos.map((todo) => ` 
   <div class="todos">
     <div class="task"><input type="checkbox" id="demoCheckbox" name="checkbox" value="1">
         <label for="demoCheckbox">${todo.description}</label>
@@ -20,4 +19,4 @@ import './style.css';
   </div>
   `).join('');
 
-  taskContainer.innerHTML = tasks;
+taskContainer.innerHTML = tasks;
