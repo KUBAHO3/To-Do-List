9bf95f4c1852136a16f7661a9de252471185173f
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-29 10:08:04 +0530

    add css as given

--- src/index.js
+++ src/index.js
@@ -12,3 +12,11 @@ function component() {
   }
   
   document.body.appendChild(component());
+
+  const todos = [
+    { index: 1, description: 'Learn JavaScript', completed: false  },
+    { index: 2, description: 'Learn HTML', completed: true },
+    { index: 3, description: 'Learn CSS', completed: false },
+    { index: 4, description: 'Learn React', completed: true },
+    { index: 5, description: 'Learn Redux', completed: false },
+  ]
