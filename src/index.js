e8c20914dcdc4889cbc6fab0bb9935e54464d159
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-30 15:21:01 +0530

    select  elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 /*eslint-disable*/
 import _ from 'lodash'; /*eslint-disable*/
 import './style.css';
+import { Operations } from './modules/operations.js';
+const taskContainer = document.getElementById('task-container');
+const submitButton = document.getElementById('addButton');
+const clearAllDone = document.getElementById('clearComplete');
+let editButtonStats=false;
+window.onload = function windowReady() {
+  Operations.showTask();
+  submitButton.onclick = function () {
+    Operations.createTask();
+  };
+
+};
