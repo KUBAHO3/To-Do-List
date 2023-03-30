2ccc7dba0f8d1db288c10770c21cd5d206c363ad
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-30 15:22:12 +0530

    select  events listeners

--- src/index.js
+++ src/index.js
@@ -11,5 +11,24 @@ window.onload = function windowReady() {
   submitButton.onclick = function () {
     Operations.createTask();
   };
-
+  taskContainer.addEventListener('click', (e) => {
+    if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
+      if (e.target.className === 'checkbox-class') {
+        const ids = e.target.id.replace('checkbox-', '');
+        const description = document.getElementById('d' + ids);
+        const data = Operations.getAllTasks();
+        const index = parseInt(ids, 10);
+        if (data !== []) {
+          if (data[index].completed) {
+            data[index].completed = false;
+            description.style.textDecoration = 'none';
+          } else {
+            data[index].completed = true;
+            description.style.textDecoration = 'line-through';
+          }
+          Operations.updateTask(data);
+        }
+      }
+    }
+  });
 };
