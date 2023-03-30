7901c7e79b51cbaa34832d3c5621bc38c56435e0
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-30 15:23:15 +0530

    add edit functionality

--- src/index.js
+++ src/index.js
@@ -31,4 +31,30 @@ window.onload = function windowReady() {
       }
     }
   });
+
+  
+// ! EDTING 
+taskContainer.addEventListener('click', (e) => {
+  if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
+    if (e.target.className === 'editButton') {
+      const ids = e.target.id.replace('editBttn-', '');
+      let description = document.getElementById('d' + ids);
+      let data = Operations.getAllTasks();
+      const index = parseInt(ids, 10);
+      let editInput = document.getElementById('edit-'+ ids);
+      description.style.display='none';
+      editInput.style.display='block';
+      if (editButtonStats !== false) {
+        data[index].description=editInput.value;
+        description.style.display='block';
+        editInput.style.display='none';
+        Operations.updateTask(data);
+        Operations.showTask();
+        editButtonStats = false;
+      }else{
+        editButtonStats = true;
+      }
+    }
+  }
+});
 };
