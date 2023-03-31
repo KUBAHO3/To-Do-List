f4762f9b3bed8fcb7d2aadb73d42aecd19102b75
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-31 08:52:35 +0530

    fine the functionalities

--- src/index.js
+++ src/index.js
@@ -4,33 +4,14 @@ import './style.css';
 import { Operations } from './modules/operations.js';
 const taskContainer = document.getElementById('task-container');
 const submitButton = document.getElementById('addButton');
-const clearAllDone = document.getElementById('clearComplete');
+// const clearAllDone = document.getElementById('clearComplete');
 let editButtonStats=false;
 window.onload = function windowReady() {
   Operations.showTask();
   submitButton.onclick = function () {
     Operations.createTask();
   };
-  taskContainer.addEventListener('click', (e) => {
-    if (e.target !== null && e.target !== 'NaN' && e.target !== '') {
-      if (e.target.className === 'checkbox-class') {
-        const ids = e.target.id.replace('checkbox-', '');
-        const description = document.getElementById('d' + ids);
-        const data = Operations.getAllTasks();
-        const index = parseInt(ids, 10);
-        if (data !== []) {
-          if (data[index].completed) {
-            data[index].completed = false;
-            description.style.textDecoration = 'none';
-          } else {
-            data[index].completed = true;
-            description.style.textDecoration = 'line-through';
-          }
-          Operations.updateTask(data);
-        }
-      }
-    }
-  });
+  
 
   
 // ! EDTING 
@@ -73,13 +54,5 @@ taskContainer.addEventListener('click', (e) => {
    }
  });
 
- clearAllDone.addEventListener('click',(e)=>{
-  const data= Operations.getAllTasks();
-  const storage=data.filter((todo)=>todo.completed===false);
-  for(let i=1; i<storage.length; i=+1) {
-    storage[i].index=i+1;
-  }
-  Operations.updateTask(storage);
-  Operations.showTask();
- })
+
 };
