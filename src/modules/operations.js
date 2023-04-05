e9f394be34372ef810b1c7d7e84f1c80d2a19504
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-05 18:26:08 +0530

    add more modules

--- src/modules/operations.js
+++ src/modules/operations.js
@@ -1,61 +1,10 @@
-function createTask() {
-  const inputData = document.getElementById('inputText').value;
-  const saveData = document.getElementById('formText');
-  const storage = getAllTasks().length+1;
-  const task = [{ description: inputData, completed: false, index: parseInt(storage) }];
-  saveTask(task);
-  showTask();
-  saveData.reset();
-}
+import { createTask } from './createTask.js';
+import { showTask } from './showTask.js';
+import { removeTask } from './removeTask.js';
+import { saveTask, getAllTasks, updateTask } from './saveTask.js';
 
-function showTask() {
-  const div = document.querySelector('#todo-container');
-  div.innerHTML = '';
-  getAllTasks().forEach((task) => {
-    let checked = '';
-    let style = '';
-    let styleDescription = '';
-    const styleText='style="display:none"';
-    if (task.completed) {
-      styleDescription = 'style="text-decoration: line-through"';
-      checked = 'checked';
-      style = 'class="line"';
-    }
-    div.innerHTML += `<li><input class="checkbox-class" type="checkbox" id="checkbox-${task.index}" ${checked} /> <h3  id="d${task.index}" ${style} ${styleDescription} >${task.description}</h3><input class="edit-class" type="text" value="${task.description}" id="edit-${task.index}" ${styleText}/><button type="button" id="editBttn-${task.index}" class="editButton"></button><button type="button" class="deleteButton" id="delete-${task.index}"></button></li>`;
-  });
-}
-
-function removeTask(index) {
-  if (index > -1) {
-    const newData = getAllTasks();
-    newData.splice(index, 1);
-    for (let i = index; i < getAllTasks().length - 1; i += 1) {
-      newData[i].index = i+1;
-    }
-    updateTask(newData);
-    showTask();
-  }
-}
-
-function saveTask(todo) {
-  if (getAllTasks().length !== 0) {
-    localStorage.setItem(
-      'data',
-      JSON.stringify(getAllTasks().concat(todo)),
-    );
-  } else {
-    localStorage.setItem('data', JSON.stringify(todo));
-  }
-}
-
-function getAllTasks() {
-  if (localStorage.getItem('data')) {
-    return JSON.parse(localStorage.getItem('data'));
-  }
-  return [];
-}
-
-const updateTask =(newDatas)=> {
-  localStorage.clear();
-  localStorage.setItem('data', JSON.stringify(newDatas));
-}
+// Call functions as needed
+createTask();
+showTask();
+removeTask(0);
+saveTask([{ description: 'New task', completed: false, index: 1 }]);
---
+++ src/modules/saveTask.js
@@ -0,0 +1,23 @@
+export function saveTask(todo) {
+    if (getAllTasks().length !== 0) {
+      localStorage.setItem(
+        'data',
+        JSON.stringify(getAllTasks().concat(todo)),
+      );
+    } else {
+      localStorage.setItem('data', JSON.stringify(todo));
+    }
+  }
+  
+  export function getAllTasks() {
+    if (localStorage.getItem('data')) {
+      return JSON.parse(localStorage.getItem('data'));
+    }
+    return [];
+  }
+  
+  export const updateTask =(newDatas)=> {
+    localStorage.clear();
+    localStorage.setItem('data', JSON.stringify(newDatas));
+  }
+  
