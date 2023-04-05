b1d61e03f66dcae4635faf2517380b0a3ece61db
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-05 18:25:12 +0530

    remove class

--- src/modules/operations.js
+++ src/modules/operations.js
@@ -1,66 +1,61 @@
-/*eslint-disable*/
-export  class Operations {
-  static createTask() {
-    const inputData = document.getElementById('inputText').value;
-    const saveData = document.getElementById('formText');
-    const storage = this.getAllTasks().length+1;
-    const task = [{ description: inputData, completed: false, index: parseInt(storage) }];
-    this.saveTask(task);
-    this.showTask();
-    saveData.reset();
-  }
-  static showTask() {
-    const div = document.querySelector('#todo-container');
-    div.innerHTML = '';
-    this.getAllTasks().forEach((task) => {
-      let checked = '';
-      let style = '';
-      let styleDescription = '';
-      const styleText='style="display:none"';
-      if (task.completed) {
-        styleDescription = 'style="text-decoration: line-through"';
-        checked = 'checked';
-        style = 'class="line"';
-      }
-      div.innerHTML += `<li><input class="checkbox-class" type="checkbox" id="checkbox-${task.index}" ${checked} /> <h3  id="d${task.index}" ${style} ${styleDescription} >${task.description}</h3><input class="edit-class" type="text" value="${task.description}" id="edit-${task.index}" ${styleText}/><button type="button" id="editBttn-${task.index}" class="editButton"></button><button type="button" class="deleteButton" id="delete-${task.index}"></button></li>`;
-    });
-  }
+function createTask() {
+  const inputData = document.getElementById('inputText').value;
+  const saveData = document.getElementById('formText');
+  const storage = getAllTasks().length+1;
+  const task = [{ description: inputData, completed: false, index: parseInt(storage) }];
+  saveTask(task);
+  showTask();
+  saveData.reset();
+}
 
-  
-  static removeTask(index) {
-    if (index > -1) {
-      const newData = this.getAllTasks();
-      newData.splice(index, 1);
-      for (let i = index; i < this.getAllTasks().length - 1; i += 1) {
-        newData[i].index = i+1;
-      }
-      this.updateTask(newData);
-      this.showTask();
+function showTask() {
+  const div = document.querySelector('#todo-container');
+  div.innerHTML = '';
+  getAllTasks().forEach((task) => {
+    let checked = '';
+    let style = '';
+    let styleDescription = '';
+    const styleText='style="display:none"';
+    if (task.completed) {
+      styleDescription = 'style="text-decoration: line-through"';
+      checked = 'checked';
+      style = 'class="line"';
     }
-  }
+    div.innerHTML += `<li><input class="checkbox-class" type="checkbox" id="checkbox-${task.index}" ${checked} /> <h3  id="d${task.index}" ${style} ${styleDescription} >${task.description}</h3><input class="edit-class" type="text" value="${task.description}" id="edit-${task.index}" ${styleText}/><button type="button" id="editBttn-${task.index}" class="editButton"></button><button type="button" class="deleteButton" id="delete-${task.index}"></button></li>`;
+  });
+}
 
-  
-  static saveTask(todo) {
-    if (this.getAllTasks().length !== 0) {
-      localStorage.setItem(
-        'data',
-        JSON.stringify(this.getAllTasks().concat(todo)),
-      );
-    } else {
-      localStorage.setItem('data', JSON.stringify(todo));
+function removeTask(index) {
+  if (index > -1) {
+    const newData = getAllTasks();
+    newData.splice(index, 1);
+    for (let i = index; i < getAllTasks().length - 1; i += 1) {
+      newData[i].index = i+1;
     }
+    updateTask(newData);
+    showTask();
   }
+}
 
-  static getAllTasks() {
-    if (localStorage.getItem('data')) {
-      return JSON.parse(localStorage.getItem('data'));
-    }
-    return [];
+function saveTask(todo) {
+  if (getAllTasks().length !== 0) {
+    localStorage.setItem(
+      'data',
+      JSON.stringify(getAllTasks().concat(todo)),
+    );
+  } else {
+    localStorage.setItem('data', JSON.stringify(todo));
   }
+}
 
-  static updateTask =(newDatas)=> {
-    localStorage.clear();
-    localStorage.setItem('data', JSON.stringify(newDatas));
+function getAllTasks() {
+  if (localStorage.getItem('data')) {
+    return JSON.parse(localStorage.getItem('data'));
   }
+  return [];
+}
 
+const updateTask =(newDatas)=> {
+  localStorage.clear();
+  localStorage.setItem('data', JSON.stringify(newDatas));
 }
